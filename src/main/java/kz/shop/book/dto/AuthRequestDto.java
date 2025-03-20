3b4e2e9a358777edb600d6ba93fcce618b45f580
alimbetov <alimbetovpost@gmail.com>
2025-03-21 00:06:48 +0500

    Revert "1"
    This reverts commit 07fcdf9d3dc4c5e9aadab61e563481413365835c.

--- src/main/java/kz/shop/book/dto/AuthRequestDto.java
+++
@@ -1,9 +0,0 @@
-package kz.shop.book.dto;
-
-import lombok.Data;
-
-@Data
-public class AuthRequestDto {
-    private String email;
-    private String password;
-}
--- src/main/java/kz/shop/book/exception/LimitExceededException.java
+++
@@ -1,7 +0,0 @@
-package kz.shop.book.exception;
-
-public class LimitExceededException extends RuntimeException {
-    public LimitExceededException(String message) {
-        super(message);
-    }
-}
